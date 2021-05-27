c9ffe00ffecd88f4f1d96a13fee8805d325a6764
Louis Smit <louissmit97@gmail.com>
2021-05-27 14:26:27 +0200

    Update intro copy

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,8 @@
-import Head from "next/head"
-import Navbar from "components/Navbar"
-import Footer from "components/Footer"
+import Head from 'next/head'
+import Navbar from 'components/Navbar'
+import Footer from 'components/Footer'
 
-export const Layout = ({ children, bg = "" }) => {
+export const Layout = ({ children, bg = '' }) => {
   return (
     <div className={bg}>
       <Head>
@@ -12,7 +12,8 @@ export const Layout = ({ children, bg = "" }) => {
         <meta property="og:description" content="Occupational Health Suite." />
         <meta
           name="description"
-          content="Heroh is the best platform for managing the health of your clients and employees. We handle thousands of records every year for forward thinking businesses in Africa."
+          // content="Heroh is the best platform for managing the health of your clients and employees. We handle thousands of records every year for forward thinking businesses in Africa."
+          content="An Occupational Health suite of tools that any practice or clinic can afford and which creates a network of independent practices, collectively outclassing the national service providers."
         ></meta>
         <meta
           property="og:site_name"
@@ -32,13 +33,13 @@ export const Layout = ({ children, bg = "" }) => {
 }
 
 export const ContentWidth = ({ children }) => (
-  <div className="relative max-w-6xl mx-auto px-2 sm:px-6 lg:px-8">
+  <div className="relative max-w-6xl px-2 mx-auto sm:px-6 lg:px-8">
     {children}
   </div>
 )
 
 export const Dark = () => (
-  <div className="absolute bg-gray-800 h-3/5 md:h-4/5 lg:h-5/6 xl:h-5/6 2xl:h-1/2 w-full transform -skew-y-3 -top-0" />
+  <div className="absolute w-full transform -skew-y-3 bg-gray-800 h-3/5 md:h-4/5 lg:h-5/6 xl:h-5/6 2xl:h-1/2 -top-0" />
 )
 
 export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,26 @@
-import Layout, { ContentWidth, Dark } from "components/Layout"
-import Contact from "components/Contact"
-import Pricing from "components/Pricing"
-import FeatureGrid from "components/Features"
+import Layout, { ContentWidth, Dark } from 'components/Layout'
+import Contact from 'components/Contact'
+import Pricing from 'components/Pricing'
+import FeatureGrid from 'components/Features'
 
 const Home = () => {
   return (
     <Layout>
       <Dark />
       <ContentWidth>
-        <div className="flex flex-col items-center pt-20 mx-auto text-center space-y-4">
-          <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold font-fira text-gray-50">
+        <div className="flex flex-col items-center pt-20 mx-auto space-y-4 text-center">
+          <h2 className="text-3xl font-extrabold sm:text-4xl md:text-5xl lg:text-6xl font-fira text-gray-50">
             Occupational Health Suite
           </h2>
 
-          <p className="text-gray-300 font-medium text-md sm:text-lg max-w-md sm:max-w-xl">
-            <b>Heroh</b> is the best platform for managing the health of your
+          <p className="max-w-md font-medium text-gray-300 text-md sm:text-lg sm:max-w-xl">
+            {/* <b>Heroh</b> is the best platform for managing the health of your
             clients and employees. We handle <b>thousands of records</b> every
             year for forward thinking businesses in Africa.
+            <br /> */}
+            A suite of tools that any practice or clinic can afford and which
+            creates a network of independent practices, collectively outclassing
+            the national service providers.
           </p>
         </div>
         <HerohImage />
@@ -25,7 +29,7 @@ const Home = () => {
         <Definition />
       </ContentWidth>
 
-      <div className="flex flex-col space-y-14 sm:space-y-36 max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 mt-28">
+      <div className="flex flex-col max-w-5xl px-2 mx-auto space-y-14 sm:space-y-36 sm:px-6 lg:px-8 mt-28">
         <InfoSectionLeft
           title="Manage Electronic Health Records"
           imgSrc="/ehr.png"
@@ -56,7 +60,7 @@ const Home = () => {
         <FeatureGrid />
         <Pricing />
       </ContentWidth>
-      <div className="bg-gray-50 mt-16">
+      <div className="mt-16 bg-gray-50">
         <Contact />
       </div>
 
@@ -66,11 +70,11 @@ const Home = () => {
 }
 
 const Definition = () => (
-  <div className="flex flex-col space-y-10 w-full items-center text-center mt-10 lg:mt-4">
-    <h3 className="text-4xl md:text-4xl lg:text-5xl font-extrabold font-fira text-gray-800">
+  <div className="flex flex-col items-center w-full mt-10 space-y-10 text-center lg:mt-4">
+    <h3 className="text-4xl font-extrabold text-gray-800 md:text-4xl lg:text-5xl font-fira">
       What is Heroh?
     </h3>
-    <p className="text-gray-700 font-medium text-md sm:text-xl sm:max-w-4xl">
+    <p className="font-medium text-gray-700 text-md sm:text-xl sm:max-w-4xl">
       <b>Heroh</b> is a collection of tools used by occupational health
       practices and their clients to manage and track employee health by issue
       of a <b>Certificate of Fitness</b>, the industry standard document that
@@ -81,15 +85,15 @@ const Definition = () => (
 )
 
 const InfoSectionLeft = ({ title, imgSrc, children }) => (
-  <div className="flex flex-col md:flex-row space-x-0 md:space-x-20 items-center">
-    <div className="flex flex-col flex-1 space-y-5 md:space-y-10 order-last md:order-first mt-12 md:mt-0">
-      <h3 className="text-4xl md:text-5xl font-black text-gray-800">{title}</h3>
+  <div className="flex flex-col items-center space-x-0 md:flex-row md:space-x-20">
+    <div className="flex flex-col flex-1 order-last mt-12 space-y-5 md:space-y-10 md:order-first md:mt-0">
+      <h3 className="text-4xl font-black text-gray-800 md:text-5xl">{title}</h3>
       <p className="text-2xl text-gray-700">{children}</p>
     </div>
     <div className="flex-1 order-first md:order-last">
       <img
         src={imgSrc}
-        className="shadow-lg rounded-xl transform rotate-0 md:rotate-2 w-3/4 md:w-auto"
+        className="w-3/4 transform rotate-0 shadow-lg rounded-xl md:rotate-2 md:w-auto"
         alt="Image of Heroh"
       />
     </div>
@@ -97,16 +101,16 @@ const InfoSectionLeft = ({ title, imgSrc, children }) => (
 )
 
 const InfoSectionRight = ({ title, imgSrc, children }) => (
-  <div className="flex flex-col md:flex-row space-x-0 md:space-x-20 items-center">
+  <div className="flex flex-col items-center space-x-0 md:flex-row md:space-x-20">
     <div className="flex-1">
       <img
         src={imgSrc}
-        className="shadow-lg rounded-xl transform rotate-0 md:-rotate-2 w-3/4 md:w-auto"
+        className="w-3/4 transform rotate-0 shadow-lg rounded-xl md:-rotate-2 md:w-auto"
         alt="Image of Heroh"
       />
     </div>
-    <div className="flex flex-col flex-1 space-y-5 md:space-y-10 mt-12 md:mt-0">
-      <h3 className="text-4xl md:text-5xl font-black text-gray-800">{title}</h3>
+    <div className="flex flex-col flex-1 mt-12 space-y-5 md:space-y-10 md:mt-0">
+      <h3 className="text-4xl font-black text-gray-800 md:text-5xl">{title}</h3>
       <p className="text-2xl text-gray-700">{children}</p>
     </div>
   </div>
@@ -117,13 +121,13 @@ const HerohImage = () => (
 )
 
 // const WhatIsACertificate = () => (
-//   <div className="bg-gray-700 rounded-2xl py-5 px-6 space-y-5 shadow-lg border border-red-400">
+//   <div className="px-6 py-5 space-y-5 bg-gray-700 border border-red-400 shadow-lg rounded-2xl">
 //     <div className="flex flex-row justify-between text-gray-100 border-b">
-//       <h4 className="text-xl font-extrabold border-gray-50 pb-4">
+//       <h4 className="pb-4 text-xl font-extrabold border-gray-50">
 //         What's a certificate?
 //       </h4>
 //       <svg
-//         className="text-gray-100 h-4"
+//         className="h-4 text-gray-100"
 //         xmlns="http://www.w3.org/2000/svg"
 //         viewBox="0 0 20 20"
 //         fill="currentColor"
